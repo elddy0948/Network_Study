5fc5753834caaa4ea69aaf007166f42f73b7fec8
Joons <elddy0948@gmail.com>
2022-11-28 20:45:05 +0900

    project init

---
+++ MySpecialTickets/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MySpecialTickets
+//
+//  Created by 김호준 on 2022/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
